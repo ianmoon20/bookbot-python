972a9d676b2e3ee376c8b06ec349ea8c5540aa61
Ian Moon <ianmoon20@gmail.com>
2025-08-07 01:45:26 -0400

    Multi-book functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_characters
+from sys import *
 
 FILE_TO_READ = "books/frankenstein.txt"
 
@@ -9,8 +10,20 @@ def get_book_text(path):
         file_contents = f.read()
     return file_contents
 
-def main():
-    contents = get_book_text(FILE_TO_READ)
-    print("============ BOOKBOT ============")
+def generate_book_report(book):
+    report = f"============ BOOKBOT ============\nAnalyzing book found at {FILE_TO_READ}...\n----------- Word Count ----------\nFound {count_words(book)} total words\n--------- Character Count -------"
+    characters_in_book = count_characters(book)
+    sorted_by_usage = sorted(characters_in_book.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    for character in sorted_by_usage:
+        report += f"\n{character[0]}: {character[1]}"
+    report += "\n============= END ==============="
+    return report
 
+def main():
+    if len(argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    FILE_TO_READ = argv[1]
+    contents = get_book_text(FILE_TO_READ)
+    print(generate_book_report(contents))
 main()
--- stats.py
+++ stats.py
@@ -4,9 +4,10 @@ def count_words(text):
 def count_characters(text):
     character_dict = {}
     for char in text:
-        character = char.lower()
-        if character in character_dict.keys():
-            character_dict[character] += 1
-        else:
-            character_dict[character] = 1
+        if char.isalnum():
+            character = char.lower()
+            if character in character_dict.keys():
+                character_dict[character] += 1
+            else:
+                character_dict[character] = 1
     return character_dict
