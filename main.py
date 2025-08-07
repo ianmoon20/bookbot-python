ffc24e93d5afebc0393e885d2a112fb8eb9b428d
Ian Moon <ianmoon20@gmail.com>
2025-08-07 02:05:19 -0400

    done

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from stats import count_characters
 from sys import *
 import os
 
+FILE_TO_READ = "books/frankenstein.txt"
+
 def get_book_text(path):
     file_contents = ""
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
-def generate_book_report(book, path):
-    report = f"============ BOOKBOT ============\nAnalyzing book found at {path}...\n----------- Word Count ----------\nFound {count_words(book)} total words\n--------- Character Count -------"
+def generate_book_report(book):
+    report = f"============ BOOKBOT ============\nAnalyzing book found at {FILE_TO_READ}...\n----------- Word Count ----------\nFound {count_words(book)} total words\n--------- Character Count -------"
     characters_in_book = count_characters(book)
     sorted_by_usage = sorted(characters_in_book.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
     for character in sorted_by_usage:
@@ -23,10 +25,10 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         exit(1)
     try:
-        file_to_read = argv[1]
-        print(file_to_read)
-        contents = get_book_text(file_to_read)
-        print(generate_book_report(contents, file_to_read))
+        FILE_TO_READ = argv[1]
+        print(FILE_TO_READ)
+        contents = get_book_text(FILE_TO_READ)
+        print(generate_book_report(contents))
     except FileNotFoundError:
         print("That file does not exist.")
     except Exception as e:
